6fc34fe3fdfa4d4a0210f46ae2f3d5f013df26d0
Ghaith Madhkour <71493702+Ghaith-M@users.noreply.github.com>
2021-02-01 16:15:55 +0100

    first

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MySecondcomponent = () => {
+  return <h2>ghaith.madhkour@gmail.com</h2>;
+};
+export default MySecondcomponent;
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MyFirstComponent = () => {
+  return <h2>Ghaith Madhkour</h2>;
+};
+export default MyFirstComponent;
